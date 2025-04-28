f2aaa729ca548245a4ae24c9692dcc840d7da250
Jaci-Xavier <devxvr@gmail.com>
2025-04-28 07:31:24 -0300

    inicia projeto

--- src/stores/useStore.js
+++ src/stores/useStore.js
@@ -1,11 +1,8 @@
-// src/stores/useStore.js
 import create from 'zustand';
 
 const useStore = create((set) => ({
-  // Estado inicial
   count: 0,
   
-  // Funções para alterar o estado
   increment: () => set((state) => ({ count: state.count + 1 })),
   decrement: () => set((state) => ({ count: state.count - 1 })),
   reset: () => set({ count: 0 }),
